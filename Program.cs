e8d50639fd243e3a93b846e096c96ff9ce07b950
Dyl4nMD <dylandaniel880@outlook.com>
2026-04-17 15:21:29 -0500

    V1: Banner de sistema de Cálculo de Materiales

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// Versión 1: Banner de Sistema
+Console.WriteLine ("-------------------------------------------------------");
+Console.WriteLine ("++++++++++++++++++ 3D SERVICES S.A. +++++++++++++++++++");
+Console.WriteLine ("++++++++++ SISTEMA DE CÁLCULO DE MATERIALES +++++++++++");
+Console.WriteLine ("-------------------------------------------------------");
