7ab3233168c099beb7cbcb54c59be6e06fdd707f
Dyl4nMD <dylandaniel880@outlook.com>
2026-04-17 15:23:30 -0500

    V2: Implementada estructura secuencial de costos

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
-﻿// Versión 1: Banner de Sistema
+﻿// Versión 2: Banner de Sistema
 Console.WriteLine ("-------------------------------------------------------");
 Console.WriteLine ("++++++++++++++++++ 3D SERVICES S.A. +++++++++++++++++++");
 Console.WriteLine ("++++++++++ SISTEMA DE CÁLCULO DE MATERIALES +++++++++++");
 Console.WriteLine ("-------------------------------------------------------");
+Console.Write("Ingrese el volumen de la pieza (ml): ");
+double volumen = double.Parse(Console.ReadLine());
+Console.Write("Ingrese el costo de la resina por ml ($): ");
+double costo_ml = double.Parse(Console.ReadLine());
+double costo_total = volumen * costo_ml;
+Console.WriteLine($"\nEl costo total de manufactura es: ${costo_total:F2}");
