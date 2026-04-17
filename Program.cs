1a760ebefbe7712aab953a3fdb77ff85834783ae
Dyl4nMD <dylandaniel880@outlook.com>
2026-04-17 15:39:03 -0500

    V4: Implementado ciclo for para procesamiento de lotes

--- Program.cs
+++ Program.cs
@@ -1,19 +1,20 @@
-﻿// Versión 3: Validador de Impresión 3D V2
+﻿// Versión 4: Gestor de Lotes de Producción
 Console.WriteLine ("-------------------------------------------------------");
 Console.WriteLine ("++++++++++++++++++ 3D SERVICES S.A. +++++++++++++++++++");
 Console.WriteLine ("++++++++++ SISTEMA DE CÁLCULO DE MATERIALES +++++++++++");
 Console.WriteLine ("-------------------------------------------------------");
-Console.WriteLine("--- Validador de Impresión 3D V2 ---");
-double capacidad_maxima = 500.0;
-Console.Write("Ingrese el volumen de la pieza (ml): ");
-double volumen = double.Parse (Console.ReadLine());
-if (volumen > capacidad_maxima) {
-Console.WriteLine("¡Error! La pieza excede la capacidad máxima de la impresora(500 ml).");
-Console.WriteLine("Sugerencia: Divida el modelo en partes más pequeñas.");
-} else {
+Console.Write("¿Cuántas piezas distintas componen el lote?: ");
+int cantidad_piezas = Convert.ToInt32(Console.ReadLine());
 Console.Write("Ingrese el costo de la resina por ml ($): ");
 double costo_ml = double.Parse(Console.ReadLine());
-double costo_total = volumen * costo_ml;
-Console.WriteLine("Estado: ¡Aprobado para impresión!");
-Console.WriteLine($"El costo total será: ${costo_total:F2}");
+double volumen_total_lote = 0.0;
+for (int i = 0; i < cantidad_piezas; i++) {
+Console.Write($"Ingrese el volumen de la pieza {i + 1} (ml): ");
+double volumen_pieza = double.Parse(Console.ReadLine());
+volumen_total_lote += volumen_pieza; // Acumulador
 }
+double costo_total_lote = volumen_total_lote * costo_ml;
+Console.WriteLine("\n--- Resumen de Producción ---");
+Console.WriteLine($"Total de piezas: {cantidad_piezas}");
+Console.WriteLine($"Volumen total requerido: {volumen_total_lote:F2} ml");
+Console.WriteLine($"Costo total del lote: ${costo_total_lote:F2}");
